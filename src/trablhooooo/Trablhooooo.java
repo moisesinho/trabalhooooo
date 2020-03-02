98ca6d692e7c9aaec9d32d0dce99876f9ff2c8f1
Moises Santos <Moises Santos@DESKTOP-OCUQ1EB>
2020-03-02 10:21:57 -0300

    Coloquei uma mensagem de bom dia

--- src/trablhooooo/Trablhooooo.java
+++ src/trablhooooo/Trablhooooo.java
@@ -15,6 +15,7 @@ public class Trablhooooo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Bom dia seila");
         // TODO code application logic here
     }
     
