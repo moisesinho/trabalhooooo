7c2c83e9682d2c52cb1475072dabe4f2982ee7f7
moisesinho <61696755+moisesinho@users.noreply.github.com>
2020-03-02 11:10:43 -0300

    nada de mais

--- src/trablhooooo/Trablhooooo.java
+++ src/trablhooooo/Trablhooooo.java
@@ -15,7 +15,7 @@ public class Trablhooooo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Bom dia seila");
+        System.out.println("Bom dia seilo");
         // TODO code application logic here
     }
     
